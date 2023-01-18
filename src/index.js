6587582a1d429d2ffb82c8e2b22603f75c0a8ffb
DzmitryBN <DzmitryBN@gmail.com>
2023-01-18 11:57:28 +0100

    init: implement solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let reversedArrDouble = [];
+  let reversedArrSingle = [];
+
+  if (arguments.length === 0){
+    return []
+  }
+
+  if (matrix.length === 0)  {
+    return []
+  }
+
+  reversedArrDouble = matrix.map(function (currentValue, index) {
+    if (index % 2 !== 0) {
+      return currentValue.sort((a, b) => b - a);
+    } else {
+      return currentValue;
+    }
+  });
+
+  reversedArrSingle = reversedArrDouble.reduce(function (prev, item) {
+    return prev.concat(item)
+  })
+
+  return reversedArrSingle;
 }
